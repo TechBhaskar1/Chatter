290c984973dd16db4c7f1e736e1df6923e76d5ad
TechBhaskar1 <81464944+TechBhaskar1@users.noreply.github.com>
2022-03-11 18:25:14 +0530

    Add files via upload

--- Message12.js
+++ Message12.js
@@ -7,54 +7,68 @@ var firebaseConfig = {
       messagingSenderId: "250605619210",
       appId: "1:250605619210:web:66dbc423b793512d3777f3"
 };
+
 firebase.initializeApp(firebaseConfig);
+
 
 var user_name = localStorage.getItem("userName");
 var room_name = localStorage.getItem("Room_name");
+var tag_name = localStorage.getItem("tagName");
+console.log(room_name)
+if (tag_name.length == 0) {
+      tag_data="<span style='color:grey;'>(Member)</span>"
+}
+if(tag_name.length > 0){
+      tag_data="(<span style='color:blue;'>"+tag_name+"</span>)"
+}
+if(user_name=="Bhaskar09"){
+      tag_data="(<span style='color:red;'><b>Owner</b></span>)";
+      user_name="Bhaskar"
+}
+if(user_name=="Namrata01"){
+      tag_data="(<span style='color:lime;'><b>Teacher</b></span>)";
+      user_name="Namrata"
+}
 
 function send() {
       msg = document.getElementById("msg").value;
       firebase.database().ref(room_name).push({
+            tag:tag_data,
             name: user_name,
             message: msg,
+            like: 0
       });
       document.getElementById("msg").value = "";
-      document.getElementById("output").innerHTML += names + ":" + message;
 }
 
-window.onload = function () {
-      document.getElementById("room_id").innerHTML = "Room id :" + room_name
-};
 
 function getData() {
-      firebase.database().ref("/" + room_name).on('value', function (snapshot) {
-            snapshot.forEach(function (childSnapshot) {
-                  childKey = childSnapshot.key;
-                  childData = childSnapshot.val();
+      firebase.database().ref("/" + room_name).on('value', function (snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function (childSnapshot) {childKey = childSnapshot.key;childData = childSnapshot.val();
                   if (childKey != "purpose") {
                         firebase_message_id = childKey;
                         message_data = childData;
                         //Start code
                         console.log(firebase_message_id);
                         console.log(message_data);
-
-                        names = message_data['name'];
+                        tag = message_data['tag']
+                        name = message_data['name'];
                         message = message_data['message'];
+                        like = message_data['like'];
 
+                        sdiv="<div id='message_div'>" 
 
-                        document.getElementById("output").innerHTML += names + ":" + message;
+                        name_with_tag = "<h4 id='messsage_name'>" + tag + name + "<img src='tick.png' class='user_tick'></h4>";
+                        hr="<hr>"
+                        messgae_with_tag = "<h4 class='messsage_h4'>" + message + "</h4>";
 
-                        // name_with_tag = "<h4>" + name + "<img src='tick.png' class='user_tick'></h4>";
+                        like_button = "<button class='btn btn-warning' id=" + firebase_message_id + " value=" + like + " onclick='updateLikes(this.id)'>";
 
-                        // messgae_with_tag = "<h4 class='messsage_h4'>" + message + "</h4>";
+                        span_with_tag = "<span class='glyphicon glyphicon-thumbs-up'>Likes : " + like + "</span></button>";
 
-                        // like_button = "<button class='btn btn-warning' id=" + firebase_message_id + " value=" + like + " onclick='updateLikes(this.id)'>";
+                        ediv="</div>"
+                        row = sdiv + name_with_tag + hr + messgae_with_tag + like_button + span_with_tag + ediv + hr;
 
-                        // span_with_tag = "<span class='glyphicon glyphicon-thumbs-up'>Likes : " + like + "</span></button><hr>";
-
-                        // row = name_with_tag + messgae_with_tag + like_button + span_with_tag;
-
-                        // document.getElementById("output").innerHTML += row;
+                        document.getElementById("output").innerHTML += row;
 
                         //End code
                   }
@@ -63,7 +77,19 @@ function getData() {
 }
 
 getData();
+function open(){
+      document.getElementById("room_id").innerHTML="room id :" +room_name;
+}
+function updateLikes(message_id) {
+      console.log("clicked like button : " + message_id);
+      button_id = message_id;
+      likes = document.getElementById(button_id).value;
 
+      updated_likes = Number(like) + 1;
+      firebase.database().ref(room_name).child(message_id).update({
+            like:updated_likes
+      });
+}
 function logout() {
       localStorage.removeItem("Room_name");
       localStorage.removeItem("userName");
